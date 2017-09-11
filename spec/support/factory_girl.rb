d96716b8fa8e3c2f2332d581aa48bad09ea6edb4
Sergey Sokolov <sokolov.sergey.a@gmail.com>
2017-09-11 23:20:19 +0300

    try travis 3

--- spec/support/factory_girl.rb
+++ spec/support/factory_girl.rb
@@ -1,6 +1,5 @@
 RSpec.configure do |config|
   config.include FactoryGirl::Syntax::Methods
 end
-puts File.expand_path('../../factories', __FILE__)
 FactoryGirl.definition_file_paths = [File.expand_path('../../factories', __FILE__)]
 FactoryGirl.find_definitions
