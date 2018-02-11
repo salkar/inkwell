0cf0bf2bce0bc4c3390d19300882e4eeeee95933
Sergey Sokolov <sokolov.sergey.a@gmail.com>
2018-02-12 01:36:10 +0300

    remove rubocop from travis

--- spec/dummy/config/environments/production.rb
+++ spec/dummy/config/environments/production.rb
@@ -62,7 +62,8 @@ Rails.application.configure do
   config.action_mailer.perform_caching = false
 
   # Ignore bad email addresses and do not raise email delivery errors.
-  # Set this to true and configure the email server for immediate delivery to raise delivery errors.
+  # Set this to true and configure the email server for immediate delivery
+  # to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
